d53003308e8151f623d2ffda8fadb131a30a0ae0
SGolan <sharon.golan@outlook.com>
2019-12-13 22:18:10 +0200

    na

--- TaskWakeTimer/Source/CTaskTimerService.cpp
+++ TaskWakeTimer/Source/CTaskTimerService.cpp
@@ -1,141 +1,67 @@
 #include <iostream> 
 #include <iterator> 
-#include <list> 
-#include <mutex> 
-#include <cstdint>
 #include <thread>
-#include <chrono>
-#include <condition_variable>
+#include "CTaskTimerService.h"
 
 using namespace std;
 
-class CTaskTimerServerThread;
+CTaskTimerService* CTaskTimerService::m_pCTaskTimerService = NULL;
 
-
-/**********************************************************
-  Timer client
-***********************************************************/
-
-/**
- * \class ITaskTimerClient
- *
- * \brief interface for a thread using a timer service
- */
-class ITaskTimerClient
+CTaskTimerService* CTaskTimerService::GetInstance()
 {
-public:
-	void Wake() { m_bTimeElapsed = true; };
-	vois SetTimePoint(chrono::steady_clock::time_point a_TimePoin) { m_TimePoint = a_TimePoint ; };
-
-	void Sleep(uint32_t a_TimeMs)
+	// CTaskTimerService is a singleton
+	if (m_pCTaskTimerService != NULL)
 	{
-		// reset signal & call timer-server's function
-		m_bTimeElapsed = false;
-		CTaskTimerServer::GetInstance()->Sleep(a_TimeMs, this);
-		// wait for timer-server to signal time to wake
-		m_cvTimeElapsed.wait(m_Mutex, (m_bTimeElapsed == true));
+		return m_pCTaskTimerService;
 	}
+	else
+	{
+		m_pCTaskTimerService = new CTaskTimerService();
+		return m_pCTaskTimerService;
+	}
+}
 
-	bool								m_bTimeElapsed	;
-	condition_variable					m_cvTimeElapsed	;
-	mutex								m_Mutex			;
-	chrono::steady_clock::time_point	m_TimePoint		;
-};
-
-
-class CTaskTimerClientThread : public ITaskTimerClient
+CTaskTimerService::CTaskTimerService() : std::thread([this] { this->ThreadFunction(); })
 {
-public:
+	m_CurrentTimeSec = 0;
+}
 
-	CTaskTimerClientThread(uint16_t aThreadIndex, uint32_t aTimeBeforeSleep, uint32_t aTimeToSleep) : std::thread([this] { this->ThreadFunction(); })
-    {
-		m_ThreadIndex     = aThreadIndex     ;
-		m_TimeBeforeSleep = aTimeBeforeSleep ;
-		m_TimeToSleep     = aTimeToSleep     ;
-	}
-
-	void ThreadFunction()
-	{
-		cout << chrono::system_clock::now() << " thread #" << m_ThreadIndex << "invoked..." << endl;
-		for (; ; )
-		{
-			this_thread::sleep_for(std::chrono::seconds(m_TimeBeforeSleep));
-			CTaskTimerServer::GetInstance()->Sleep(m_TimeToSleep);
-			
-			return;
-		}
-
-	}
-	uint16_t m_ThreadIndex		;
-	uint32_t m_TimeBeforeSleep	;
-	uint32_t m_TimeToSleep		;
-};
-
-
-/**********************************************************
-  Timer server
-***********************************************************/
-
-// class of timer-server thread (used by timer server class)
-class CTaskTimerServerThread
+void CTaskTimerService::ThreadFunction()
 {
-public:
 
-};
-
-// singleton class : timer server
-class CTaskTimerServer
-{
-public:
-
-	// singleton get-instance 
-	static CTaskTimerServer* GetInstance()
+	for (; ; )
 	{
-		if (m_pCTaskTimerServer)
+		// sleep 1sec (simulates IRQ every 1sec)
+		this_thread::sleep_for(chrono::seconds(1));
+		
+		// update current time
+		m_CurrentTimeSec++;
+		
+		// scan list and signal all threads having their sleep time expired
+		for (list<CTimerItem*>::iterator iter = m_ListCTimerItem.begin(); iter != m_ListCTimerItem.end(); ++iter)
 		{
-			return m_pCTaskTimerServer;
-		}
-		else
-		{
-			m_pCTaskTimerServer = new CTaskTimerServer();
-			return m_pCTaskTimerServer;
-		}
-	}
-	// Sleep interface function (to be called by client threads)
-	void Sleep(ITaskTimerClient *a_pITaskTimerClient, uint32_t a_TimeMs)
-	{
-		chrono::steady_clock::time_point CurrentTimePoint = chrono::steady_clock::now();
-		a_pITaskTimerClient->SetTimePoint( CurrentTimePoint + chrono::seconds(a_TimeMs) );
-		m_listWaitingTasks.push_back(a_pITaskTimerClient);
-	}
-
-
-	void ThreadFunction()
-	{
-		cout << "CTaskTimerServerThread:: thread invoked" << endl;
-		for (; ; )
-		{
-			// sleep 1 sec
-			this_thread::sleep_for(chrono::seconds(1));
-			// read time and scan 
-			chrono::steady_clock::time_point CurrentTimePoint = chrono::steady_clock::now();
-			for (list<ITaskTimerClient>::iterator iter = m_listWaitingTasks.begin(); iter != m_listWaitingTasks.end(); ++iter)
+			if (iter->m_TimeToAwakeSec <= m_CurrentTimeSec)
 			{
-				if (iter->GetTimetoWake() <= CurrentTimePoint)
-				{
-					iter->Wake() = true;
-				}
+				iter->m_CSemaphore.Signal();
 			}
 		}
-
 	}
 
-private:
+}
 
-	CTaskTimerServer() : std::thread([this] { this->ThreadFunction(); }) {  };
 
-	static CTaskTimerServer*	m_pCTaskTimerServer;
-	list <ITaskTimerClient*>	m_listWaitingTasks;
-	thread						m_thread;
-
-};
+void CTaskTimerService::Sleep(uint32_t a_TimeToSleepSec)
+{
+	// calculate time to wake
+	uint32_t TimeSToWakeSec = m_CurrentTimeSec + a_TimeToSleepSec;
+	// create timer list-item
+	CTimerItem *pCTimerItem    = new CTimerItem(TimeSToWakeSec);
+	
+	// insert item to waiting threads list
+	m_ListCTimerItemMutex.lock();
+	m_ListCTimerItem.push_back(pCTimerItem);
+	m_ListCTimerItemMutex.unlock();
+	
+	// block calling thread till wakeup signal
+	pCTimerItem->m_CSemaphore.wait();
+}
--- TaskWakeTimer/Source/main.cpp
+++ TaskWakeTimer/Source/main.cpp
@@ -1,6 +1,56 @@
 #include <iostream>
 
+using namespace std;
+
+#include "CTaskTimerService.h"
+
+
+class CTaskTimerClientThread
+{
+
+public:
+
+	CTaskTimerClientThread(	uint16_t aThreadIndex		, 
+							uint32_t aTimeBeforeSleep	,
+							uint32_t aTimeToSleep		) : std::thread([this] { this->ThreadFunction(); })
+	{
+		m_ThreadIndex     = aThreadIndex	;
+		m_TimeBeforeSleep = aTimeBeforeSleep;
+		m_TimeToSleep     = aTimeToSleep	;
+	}
+
+private:
+
+	void CTaskTimerClientThread::ThreadFunction()
+	{
+		cout << chrono::system_clock::now() << " thread #" << m_ThreadIndex << " invoked..." << endl;
+		for (; ; )
+		{
+			// sleep for a while
+			cout << chrono::system_clock::now() << " thread #" << m_ThreadIndex << " going to sleep" << endl;
+			this_thread::sleep_for(std::chrono::seconds(m_TimeBeforeSleep));
+			// wake up and "request" CTaskTimerServer to wake this thread again after m_TimeToSleep 
+			CTaskTimerServer::GetInstance()->Sleep(m_TimeToSleep);
+			cout << chrono::system_clock::now() << " thread #" << m_ThreadIndex << " awaken" << endl;
+
+			return;
+		}
+
+	}
+	uint16_t m_ThreadIndex;
+	uint32_t m_TimeBeforeSleep;
+	uint32_t m_TimeToSleep;
+};
+
 
 int main()
 {
+	// thread #0: goes to sleep @ t=2 and asks to be awaken after 4sec, i.e. @ t=6
+	CTaskTimerClientThread	thread0(0, 2, 4);
+	// thread #1: goes to sleep @ t=3 and asks to be awaken after 5sec, i.e. @ t=8
+	CTaskTimerClientThread	thread0(1, 3, 5);
+	// thread #2: goes to sleep @ t=1 and asks to be awaken after 2sec, i.e. @ t=3
+	CTaskTimerClientThread	thread0(2, 1, 2);
+
+	return 0;
 }
