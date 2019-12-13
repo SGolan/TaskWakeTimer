1f3edb5709dafadcdd83ab70d6716ea8da70cd77
SGolan <sharon.golan@outlook.com>
2019-12-14 00:50:28 +0200

    adding print time function

--- TaskWakeTimer/Source/main.cpp
+++ TaskWakeTimer/Source/main.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 std::chrono::system_clock::time_point start;
 
+
 class CTaskTimerClientThread
 {
 
@@ -21,43 +22,55 @@ public:
 	{
 	}
 
+	void Join() { m_thread.join();  }
+
+	static std::mutex	m_coutMutex;
+
 private:
+
+	void PrintTime(uint32_t a_ThreadIndex, const char* a_strState)
+	{
+		m_coutMutex.lock();
+		cout << "t = " << chrono::duration_cast<std::chrono::seconds>(chrono::system_clock::now() - start).count() << ": thread # " << a_ThreadIndex << a_strState << endl;
+		m_coutMutex.unlock();
+	}
 
 	void ThreadFunction()
 	{
-
-		cout << "t = " << chrono::duration_cast<std::chrono::seconds>(chrono::system_clock::now()-start).count()     << ": thread #" << m_ThreadIndex << " invoked..." << endl;
-
-		// sleep for a while
-		cout << "t = " << chrono::duration_cast<std::chrono::seconds>(chrono::system_clock::now()-start).count()  << ": thread #" << m_ThreadIndex << " going to sleep" << endl;
+		// be idle for m_TimeBeforeSleep seconds
 		Sleep(m_TimeBeforeSleep*1000);
 		// wake up and "request" CTaskTimerServer to wake this thread again after m_TimeToSleep 
+		PrintTime(m_ThreadIndex, " going to sleep");
 		CTaskTimerService::GetInstance()->Sleep(m_TimeToSleep);
-		cout << "t = " << chrono::duration_cast<std::chrono::seconds>(chrono::system_clock::now() - start).count() << ": thread #" << m_ThreadIndex << " awaken" << endl;
-		
-		for (; ; )
-		{
-			Sleep(1000);
-		}
-
+		PrintTime(m_ThreadIndex, " awakes");
 	}
-	uint16_t	m_ThreadIndex;
-	uint32_t	m_TimeBeforeSleep;
-	uint32_t	m_TimeToSleep;
-	std::thread	m_thread;
+	uint16_t			m_ThreadIndex;
+	uint32_t			m_TimeBeforeSleep;
+	uint32_t			m_TimeToSleep;
+	std::thread			m_thread;
 };
+
+std::mutex	CTaskTimerClientThread::m_coutMutex;
 
 
 int main()
 {
 	start = std::chrono::system_clock::now();
 
-	// thread #0: goes to sleep @ t=2 and asks to be awaken after 4sec, i.e. @ t=6
+	// thread #0: goes to sleep @ t=2 and asks to be awaken after 6sec, i.e. @ t=8
 	CTaskTimerClientThread	thread0(0, 2, 6);
-	// thread #1: goes to sleep @ t=3 and asks to be awaken after 5sec, i.e. @ t=8
-	//CTaskTimerClientThread	thread0(1, 3, 5);
+	// thread #1: goes to sleep @ t=3 and asks to be awaken after 1sec, i.e. @ t=4
+	CTaskTimerClientThread	thread1(1, 3, 1);
 	// thread #2: goes to sleep @ t=1 and asks to be awaken after 2sec, i.e. @ t=3
-	//CTaskTimerClientThread	thread0(2, 1, 2);
+	CTaskTimerClientThread	thread2(2, 1, 2);
+
+
+	thread0.Join();
+	thread1.Join();
+	thread2.Join();
+
+	cout << endl << "Press ENTER to exit..." << endl;
+	getchar();
 
 	return 0;
 }
